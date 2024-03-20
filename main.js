53c05157fbebf1788e89654aa66e47fd6aff7927
Abdulrehman <you@example.com>
2024-03-20 15:48:48 +0500

    Question 05

---
+++ main.js
@@ -0,0 +1,8 @@
+/*find a quote from a famous person you admire. print the quote and the name of its Author. your output should
+look something like the following. including the quotation marks:
+Hazrat Muhammad SAW one said “Those people who show no mercy will receive no mercy from Allah.”
+*/
+const famousPerson = "Hazrat Muhammad SAW";
+const message = `$(famousPerson)`;
+console.log(famousPerson);
+export {};
---
+++ main.ts
@@ -0,0 +1,10 @@
+/*find a quote from a famous person you admire. print the quote and the name of its Author. your output should 
+look something like the following. including the quotation marks:
+Hazrat Muhammad SAW one said “Those people who show no mercy will receive no mercy from Allah.” 
+*/
+
+const famousPerson: string = "Hazrat Muhammad SAW";
+
+const message: string = `$(famousPerson)one said “Those people who show no mercy will receive no mercy from Allah."`
+
+console.log(famousPerson)
